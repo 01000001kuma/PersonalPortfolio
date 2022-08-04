4e5bf18ebdaa205ce6ded5372567ae2102edfc97
01000001kuma <a_illera@yahoo.com>
2022-08-05 01:23:15 +0200

    wip bug

--- App.d36a57b6.js
+++ App.d36a57b6.js
@@ -38322,7 +38322,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "45123" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "34295" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
