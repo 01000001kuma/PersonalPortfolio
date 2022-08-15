8dfaced143f31cb8caf8e39bdd7c09a3211bcc5e
01000001kuma <a_illera@yahoo.com>
2022-08-15 15:01:54 +0200

    deploy bugs fix

--- dist/App.d36a57b6.js
+++ dist/App.d36a57b6.js
@@ -38150,7 +38150,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "44419" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "34049" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
